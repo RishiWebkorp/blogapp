13121cab8763f8cacf4631db8cecf973b98635f0
RishiWebkorp <rishi@webkorps.com>
2022-09-29 21:16:26 +0530

    search

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "admin@example.com",
+password:"password",
+name:"admin",
+role: User.roles[:admin]
+)
+
+User.create(email:"john@example.com",
+password:"123456",
+password_confirmation: "123456",
+name:"john"
+)
+
+10.times do |x|
+  post = Post.create(title:"Title #{x}",
+    body: "Body #{x} Words go here Idk",
+    user_id: User.first.id
+  )
+
+  5.times do |y|
+    Comment.create(body: "Comment #{y}",
+                  user_id: User.second.id,
+                  post_id: post.id)
+  end
+end
